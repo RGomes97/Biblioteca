29f38f2f25c25da03f6682d9c6b471a1f88e228d
ragostinho <ragostinho@DTERAGOSTINHOO.dte.local>
2017-05-09 19:30:27 -0300

    Alterações na estrutura

---
+++ Biblioteca/src/DAO/PedidoDAO.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import persistencia.GerenteConexao;
+
+public class PedidoDAO {
+		
+	private Connection conn;
+	
+	public PedidoDAO(){
+		conn = GerenteConexao.getConexao();
+	}
+	
+	public void addItemPedido(int idLivro, int idUsuario){
+		try {
+        	PreparedStatement ps = null;
+        	String sql = "INSERT INTO ITEM_PEDIDO(id_livro,id_usuario,data_reserva) VALUES(?,?,?)";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, idLivro);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+	
+}
---
+++ Biblioteca/src/servicos/Reserva.java
@@ -0,0 +1,23 @@
+package servicos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Livro;
+import classes.User;
+import persistencia.GerenteConexao;
+	
+
+public class Reserva {
+	
+	
+	public void reservar(User user, Livro livro){
+		int idUser = user.getId();
+		int idLivro = livro.getId();
+	}
+}
