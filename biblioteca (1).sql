ae803dac12f8cff5f9322fee1d97aa67b4eea81e
RGomes97 <rubensgagostinho@outlook.com>
2017-05-10 22:04:07 -0300

    Update biblioteca (1).sql

--- biblioteca (1).sql
+++ biblioteca (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05-Maio-2017 às 16:26
+-- Generation Time: 11-Maio-2017 às 03:03
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -29,16 +29,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `item_pedido` (
   `id_livro` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `data_pedido` datetime NOT NULL,
-  `id_pedido` int(11) NOT NULL
+  `data_reserva` datetime NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `data_retirada` datetime DEFAULT NULL,
+  `data_devolucao` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `item_pedido`
 --
 
-INSERT INTO `item_pedido` (`id_livro`, `id_usuario`, `data_pedido`, `id_pedido`) VALUES
-(1, 1, '2017-05-10 00:00:00', 1);
+INSERT INTO `item_pedido` (`id_livro`, `id_usuario`, `data_reserva`, `id_pedido`, `data_retirada`, `data_devolucao`) VALUES
+(1, 1, '2017-05-10 00:00:00', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -49,20 +51,23 @@ INSERT INTO `item_pedido` (`id_livro`, `id_usuario`, `data_pedido`, `id_pedido`)
 CREATE TABLE `livros` (
   `id` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
-  `quantidade` int(11) NOT NULL,
+  `estoque` int(11) NOT NULL,
   `autor` varchar(200) NOT NULL,
   `isbn` int(11) NOT NULL,
   `genero` varchar(200) NOT NULL,
-  `data_de_cadastro` datetime NOT NULL
+  `data_de_cadastro` datetime NOT NULL,
+  `quantidade_disponivel` int(11) NOT NULL,
+  `quantidade_reservada` int(11) NOT NULL,
+  `url` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `livros`
 --
 
-INSERT INTO `livros` (`id`, `nome`, `quantidade`, `autor`, `isbn`, `genero`, `data_de_cadastro`) VALUES
-(1, 'sdasdasda', 123321, 'sdasda', 132132, 'sdasdaasd', '2017-05-05 00:00:00'),
-(2, 'sdasda', 321321, 'sdasda', 321321, 'sdasdasda', '2017-05-05 09:44:07');
+INSERT INTO `livros` (`id`, `nome`, `estoque`, `autor`, `isbn`, `genero`, `data_de_cadastro`, `quantidade_disponivel`, `quantidade_reservada`, `url`) VALUES
+(1, 'Livro 1', 123321, 'sdasda', 132132, 'sdasdaasd', '2017-05-05 00:00:00', 123311, 0, 'https://meusdemonioscantam.files.wordpress.com/2011/01/leitura-e-animac3a7c3a3o-cultural.jpg'),
+(2, 'Livro 2', 321320, 'sdasda', 321321, 'sdasdasda', '2017-05-05 09:44:07', 0, 0, 'http://www.consoloecardinali.com.br/blog/img/lightbox/cc_edi_capa_como_se_faz_1301183768.jpg');
 
 -- --------------------------------------------------------
 
@@ -89,9 +94,7 @@ INSERT INTO `usuarios` (`id`, `nome`, `ra`, `telefone`, `tipo`, `senha`, `curso`
 (1, 'sdasda', 'sdasda', 1231321, 'sdasad', 'sdasda', 'sdasda', '0000-00-00 00:00:00'),
 (2, 'sdasda', 'dsaads', 123, 'sdasda', 'sdasda', 'sdasda', '0000-00-00 00:00:00'),
 (3, 'Rubens', '1231232', 123231231, 'usuario comum', '321321', 'ADS', '0000-00-00 00:00:00'),
-(4, 'sdasdasda', '13321', 321321321, '321321', '321321', '321321', '0000-00-00 00:00:00'),
-(5, 'Vanderson', '123231231', 213312123, 'sdasdasda', '123', 'sdasdasda', '0000-00-00 00:00:00'),
-(6, 'sdasda', '123132', 32132132, '132132', '23132132133213213211', '32132321', '2017-05-05 09:50:51');
+(7, 'admin', 'admin', 12123, 'admin', 'abc001', 'ads', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -135,7 +138,7 @@ ALTER TABLE `livros`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- Constraints for dumped tables
 --
